c06b3abeb916dc3556f2a64b97b5e11ae82bb90e
pranay202 <pranaykharabe20@gmail.com>
2021-09-03 22:22:59 +0530

    added smooth scroll

--- js/app.js
+++ js/app.js
@@ -96,3 +96,14 @@
     })
 
 })();
+
+
+//added smooth scrolling when clicking any anchor link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
